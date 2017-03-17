a0e8071aba1ecd73148b5dae9ae718dd3baf6588
Andrew Norman <anorman728@gmail.com>
2017-03-17 14:57:13 -0500

    Corrected more Ant/Ant 02 stuff.

--- Ant/Properties/AssemblyInfo.cs
+++ Ant/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ant 02")]
+[assembly: AssemblyTitle("Ant")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ant 02")]
+[assembly: AssemblyProduct("Ant")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
